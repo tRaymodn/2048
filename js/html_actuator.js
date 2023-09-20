8b50db894107105546721111987c8ed0d11e2758
Tucker Raymond <teraymond@wpi.edu>
2023-09-20 16:36:08 -0400

    Made 5x5 work

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -71,7 +71,7 @@ HTMLActuator.prototype.addTile = function (tile) {
     // Make sure that the tile gets rendered in the previous position first
     window.requestAnimationFrame(function () {
       classes[2] = self.positionClass({ x: tile.x, y: tile.y });
-      self.applyClasses(wrapper, classes, {x: tile.x+1, y: tile.y+1}); // Update the position
+      self.applyClasses(wrapper, classes, {x: tile.x, y: tile.y}); // Update the position
     });
   } else if (tile.mergedFrom) {
     classes.push("tile-merged");
