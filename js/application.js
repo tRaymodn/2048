727a95df56640e8794272119ec59abcec4a16c99
Tucker Raymond <teraymond@wpi.edu>
2023-10-08 16:02:39 -0400

    add AutoMove function and button

--- js/application.js
+++ js/application.js
@@ -1,4 +1,23 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+let game;
 window.requestAnimationFrame(function () {
-  new GameManager(5, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  game = new GameManager(5, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
+
+let interval;
+let dir = 0;
+let autoMoving = false;
+
+document.getElementById('autoMove').addEventListener("click", () => {
+  if(!autoMoving){
+    interval = setInterval(() => {
+       game.move(dir%4);
+       dir++;
+    }, 93);
+    autoMoving = true;
+  }
+  else{
+    clearInterval(interval)
+    autoMoving = false;
+  }
+})
